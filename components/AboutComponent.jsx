4324fa1fc98d48719d08f6b3eab74de798b627ed
GABRIEL FELLIPE MARQUARDT <gabriel_marquardt@estudante.sesisenai.org.br>
2023-08-25 09:05:33 -0300

    a

--- components/AboutComponent.jsx
+++ components/AboutComponent.jsx
@@ -6,9 +6,29 @@ export default function AboutComponent() {
                 <div className="flex justify-center bg-gradient-to-b  from-orange-500 to-pink-800 h-screen font-montserrat font-semibold">
                     <div className="w-1/2 font-montserrat text-white rounded-xl">
                         <div className="flex justify-center md:flex flex-col items-center pt-24">
-                            <div className="w-[300px] flex flex-col items-center md:w-full text-3xl pb-4 md:flex-row gap-3 justify-center">
-                                <p>a</p>
+                            <div className="w-full text-3xl">
+                                <div className="flex flex-col items-center justify-center"  >
+                                    <p className="pt-10">O que é a Apoia.me?</p>
+                                    <p className="pt-10 text-base text-center">No Apoia.me, acreditamos no poder da comunidade para transformar ideias em realidade. Somos uma plataforma de vaquinhas online dedicada a unir pessoas com paixões e projetos a apoiadores generosos que desejam fazer a diferença. Desde causas sociais até empreendimentos criativos, fornecemos a plataforma onde sonhos ganham vida. Nossa missão é oferecer uma maneira simples para arrecadar fundos e conquistar apoio para o que realmente importa para você. </p>
+                                </div>
                             </div>
+                            <div className="mt-10 w-full h-[10rem] bg-black rounded-3xl flex justify-center gap-20">
+                                    <div className=" flex flex-col justify-center items-center">
+                                        <p>2M+</p>
+                                        <p>Vaquinhas</p>
+                                    </div>
+                                    <div className="w-[0.1px] h-[6rem] bg-white"></div>
+                                    <div className="flex flex-col justify-center items-center">
+                                        <p>1M+</p>
+                                        <p>Usuários</p>
+                                    </div>
+                                    <div className="flex   w-[0.1px] h-[6rem] bg-white"></div>
+
+                                    <div className="flex flex-col justify-center items-center">
+                                        <p>3M+</p>
+                                        <p>Arrecadados</p>
+                                    </div>
+                                </div>
                         </div>
                     </div>
                 </div>
